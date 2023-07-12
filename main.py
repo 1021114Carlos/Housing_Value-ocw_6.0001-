8c771db52fa99fe475e1bc27d67ac50e6b6ba8fc
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2023-07-12 09:38:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
-#Part A: house hunting
-#how many months needed to save enough for down payment?
-#def houseHunting(total_cost, annual_salary, portion_saved):
-#    number_of_months = 1
-#    portion_down_payment = total_cost*(25/100)
-#    rate_of_return = (4/100)
-#    savings = 0
-#    monthly_savings = savings
-#    months = True
-#    while(months):
-#        investing = monthly_savings + (monthly_savings*(rate_of_return/12))
-#        monthly_savings = investing + (annual_salary/12)*portion_saved
-#        if(portion_down_payment>= monthly_savings):
-#            number_of_months+=1
-#        else:
-#            months=False
-#            print(number_of_months//12 "number of years to save for down payment")
-#    return(number_of_months)
-#houseHunting(float(input()), float(input()), float(input()))
+#Part A: house hunting (Ps1a.py)
+how many months needed to save enough for down payment?
+def houseHunting(total_cost, annual_salary, portion_saved):
+    number_of_months = 1
+    portion_down_payment = total_cost*(25/100)
+    rate_of_return = (4/100)
+    savings = 0
+    monthly_savings = savings
+    months = True
+    while(months):
+        investing = monthly_savings + (monthly_savings*(rate_of_return/12))
+        monthly_savings = investing + (annual_salary/12)*portion_saved
+        if(portion_down_payment>= monthly_savings):
+            number_of_months+=1
+        else:
+            months=False
+            print(number_of_months//12 "number of years to save for down payment")
+    return(number_of_months)
+houseHunting(float(input()), float(input()), float(input()))
 
-#Part B: Saving for down payment, with a percentage raise every six months
+#Part B: Saving for down payment, with a percentage raise every six months (Ps1b.py)
 
-#def houseHunting(total_cost, annual_salary, portion_saved, semi_annual_raise):
-#    number_of_months = 1
-#    portion_down_payment = total_cost*(25/100)
-#    rate_of_return = (4/100)
-#    monthly_savings = 0
-#    monthly_salary = annual_salary/12
-#    multiplicand = 6*1
-#    product = multiplicand
-#    months = True
-#    while(months):
-#       investing = monthly_savings + (monthly_savings*(rate_of_return/12))
-#       monthly_savings = investing + monthly_salary*portion_saved
-#        if(portion_down_payment>= monthly_savings):
-#            number_of_months+=1
-#            #product = multiplicand
-#            if(number_of_months == product):
-#                monthly_salary+= + monthly_salary*semi_annual_raise
-#                #monthly_savings = monthly_salary
-#                product+=multiplicand
-#        else:
-#            months=False
-#            print(number_of_months, "number of years that would take to save for down payment.")
-#    return(number_of_months)
-#houseHunting(float(input()), float(input()), float(input()), float(input()))
+def houseHunting(total_cost, annual_salary, portion_saved, semi_annual_raise):
+    number_of_months = 1
+    portion_down_payment = total_cost*(25/100)
+    rate_of_return = (4/100)
+    monthly_savings = 0
+    monthly_salary = annual_salary/12
+    multiplicand = 6*1
+    product = multiplicand
+    months = True
+    while(months):
+       investing = monthly_savings + (monthly_savings*(rate_of_return/12))
+       monthly_savings = investing + monthly_salary*portion_saved
+        if(portion_down_payment>= monthly_savings):
+            number_of_months+=1
+            #product = multiplicand
+            if(number_of_months == product):
+                monthly_salary+= + monthly_salary*semi_annual_raise
+                #monthly_savings = monthly_salary
+                product+=multiplicand
+        else:
+            months=False
+            print(number_of_months, "number of years that would take to save for down payment.")
+    return(number_of_months)
+houseHunting(float(input()), float(input()), float(input()), float(input()))
 
-#part C: Finding the right (best saving rate) amount to save away for a down payment in 36 months or less.
+#part C: Finding the right (best saving rate) amount to save away for a down payment in 36 months or less. (Ps1c.py)
 
 def savingRate(, ,):
     future_homeOwner=float(input())
